f9dd0e9ed189376887a9a168476f61dd8299a04b
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-10-21 22:07:01 -0400

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,7 +1,11 @@
-CREATE TABLE IF NOT EXISTS players (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    identifier VARCHAR(50) NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=
+CREATE TABLE `players` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`identifier` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`firstname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`lastname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `identifier` (`identifier`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
