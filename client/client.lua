ee9cf48520c4376b29957e72e2ede9e98a105d06
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2023-10-21 22:09:13 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,35 +1,73 @@
 local isNear = false
 
-local point = lib.points.new({ 
+local point = lib.points.new({
 	coords = Config.ChangeNameLoc.coords,
-	distance = 2.0
+	distance = 2.0,
 })
+
+
 
 function point:onEnter()
 	isNear = true
-	lib.showTextUI('Press [E] to change your name.', {
-		position = "right-center",
+	lib.showTextUI('[E] - Change your name', {
+		position = "top-center",
 		icon = 'user',
 		style = {
 			borderRadius = 0,
 			backgroundColor = '#48BB78',
-			color = '#white',
+			color = 'white'
 		}
 	})
 	TriggerEvent('names:changeName')
 end
 function point:onExit()
-	isNear = false
 	lib.hideTextUI()
+	isNear = false
 end
+RegisterNetEvent('names:changeName')
+AddEventHandler('names:changeName', function()
+	Citizen.CreateThread(function()
+		while true do
+			Citizen.Wait(0)
+				
+			if isNear then                
+				if IsControlJustReleased(0, 38) then
+					local input = lib.inputDialog('City Hall: Change your name.', {
+						{type = 'input', label = 'First Name', description = 'Change your First Name', required = true, min = 3, max = 10, allowCancel = true},
+						{type = 'input', label = 'Last Name', description = 'Change your Last Name', required = true, min = 3, max = 10, allowCancel = true},
+					})
+					if input then
+						local firstname = input[1]
+						local lastname = input[2]
+						if lib.alertDialog({
+							header = 'City Hall',
+							content = 'Confirm your name change to ' .. input[1] .. ' ' .. input[2] .. '.',
+							centered = true,
+							cancel = true
+						}) then
+							TriggerServerEvent('names:newName', firstname, lastname)
+						else    
+							lib.notify({
+								title = 'City Hall',
+								description = 'You have cancelled your name change.',
+								type = 'error'
+							})
+						end
+					end
+				end
+			end
+		end
+	end)
+end)
 
 
+-- register a client event that notifies the player they have a new name
 RegisterNetEvent('names:notify:NameChanged')
 AddEventHandler('names:notify:NameChanged', function(firstname, lastname)
-	-- notify the player they have a new name
+	-- notify the player that they have a new name
 	lib.notify({
 		title = 'City Hall',
-		description = 'You\'re new name is ' .. firstname .. ' ' .. lastname,
+		description = 'You\'re new name is ' .. firstname .. ' ' .. lastname .. '.',
 		type = 'success',
 		position = 'center-right'
 	})
@@ -37,6 +75,7 @@ end)
 
 RegisterNetEvent('names:notify:NameDisplay')
 AddEventHandler('names:notify:NameDisplay', function(firstName, lastName)
+	-- notify the player that they have a new name
 	local firstName = firstName
 	local lastName = lastName
 	lib.notify({
@@ -48,14 +87,17 @@ AddEventHandler('names:notify:NameDisplay', function(firstName, lastName)
 			backgroundColor = '#313131',
 			color = '#ffffff',
 			['.description'] = {
-				color = '#ffffff',
+			  color = '#ffffff'
 			}
 		},
 		icon = 'check',
-		iconColor = '#03C5AC'
+		iconColor = '#03c5ac'
 	})
 end)
 
+
+
+-- create a blip on the map for the City Hall location
 Citizen.CreateThread(function()
 	for _, info in pairs(Config.CityHallBlip) do
 		local blip = AddBlipForCoord(info.coords)
@@ -68,40 +110,4 @@ Citizen.CreateThread(function()
 		AddTextComponentString(info.name)
 		EndTextCommandSetBlipName(blip)
 	end
-end)
-
-RegisterNetEvent('names:changeName')
-AddEventHandler('names:changeName', function()
-	Citizen.CreateThread(function()
-		while true do
-			Citizen.Wait(0) -- wait 0 milliseconds
-			if isNear then
-				if IsControlJustReleased(0, 38) then -- If 'E' is pressed
-					local input = lib.inputDialog('City Hall: Change your name.', {
-						{type = 'input', labe = 'First Name', description = 'Change your First Name.', required = true, min = 3, max = 10},
-						{type = 'input', labe = 'Last Name', description = 'Change your Last Name.', required = true, min = 3, max = 10},
-					})
-					if input then
-						local firstname = input[1]
-						local lastname = input[2]
-						if lib.alertDialog({
-							header = 'City Hall',
-							content = 'Confirm  your name change to ' .. input[1] .. ' ' .. input[2] .. '.',
-							centered = true,
-							cancel = true,
-						}) then
-							TriggerServerEvent('names:newName', firstname, lastname)
-						else
-							lib.notify({
-								title = 'City Hall',
-								description = 'You have canceled your name change.',
-								type = 'error',
-								position = 'center-right'
-							})
-						end
-					end
-				end
-			end
-		end
-	end)
 end)
